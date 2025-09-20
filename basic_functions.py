6fe3d71be4a12e18b3c68c1644303e491c98d1a8
Hassani Chaima <hassani.chaima18@gmail.com>
2025-09-20 22:56:33 +0100

    another new update

--- basic_functions.py
+++ basic_functions.py
@@ -5,7 +5,7 @@ import cv2 as cv
 img = cv.imread('images/Cat.jpg')
 cv.imshow('Cat', img)
 
-def convert_to_grayscale(image_path):
+""" def convert_to_grayscale(image_path):
     # Read the image
     image = cv.imread(image_path)
     
@@ -27,7 +27,13 @@ cv.imshow("Grayscale Image", gray_image)
 
 # Save the grayscale image  
 cv.imwrite("Output/Cat_gray.jpg", gray_image)
-print(f"Image saved to Output/Cat_gray.jpg")
+print(f"Image saved to Output/Cat_gray.jpg") """
+
+#Blur image 
+blur = cv.GaussianBlur(img, (7,7), cv.BORDER_DEFAULT)
+cv.imshow('Blurred', blur)
+
+
 
 cv.waitKey(0)
 cv.destroyAllWindows()
