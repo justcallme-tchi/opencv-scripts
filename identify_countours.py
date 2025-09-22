4d00b5cb9847d6493ba460acbd573d9c329b8a11
Hassani Chaima <hassani.chaima18@gmail.com>
2025-09-22 21:11:35 +0100

    21 sep another new update

--- identify_countours.py
+++ identify_countours.py
@@ -1,4 +1,17 @@
 import cv2 as cv
 
-img = cv.imread('images/squirrel.jpg')  # Read image from file
-cv.imshow('Squirrel', img)      
+img = cv.imread('images/squirrel.jpg') 
+     
+def rescaleFrame(frame, scale=0.1):
+    width = int(frame.shape[1] * scale)   
+    height = int(frame.shape[0] * scale)  
+    dimensions = (width, height)
+    return cv.resize(frame, dimensions, interpolation=cv.INTER_AREA)
+
+img = rescaleFrame(img,0.1)
+cv.imshow('Squirrel', img) 
+
+
+
+cv.waitKey(0)
+cv.destroyAllWindows()
